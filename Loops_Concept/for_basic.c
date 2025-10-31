426537def5e1849579caf08a101ff686842e8e6a
Md Basit Azam <59113743+mdbasit897@users.noreply.github.com>
2025-11-01 01:28:10 +0530

    Added For and While loop syntax.

---
+++ Loops_Concept/for_basic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    // for(int i = 1; i>0; i++) {
+    //     printf("Condition fullfils for %d\n",i);
+    // }
+    int i=1;
+    while(i>0) {
+        printf("Condition fullfils for %d\n",i);
+        i++;
+    }
+    return 0;
+}
