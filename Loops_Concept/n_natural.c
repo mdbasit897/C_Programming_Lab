7bdee5b8ebebdd4efa1470c8aad6c92c63784ca0
mdbasit897 <mdbasit.dev@gmail.com>
2025-10-31 20:10:32 +0000

    n_natural number program has been added

---
+++ Loops_Concept/n_natural.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int i=1, sum = 0,n;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+
+    // while(i<=n){
+    //     printf("%d+",i);
+    //     i++;
+    //     sum+=i; 
+    // }
+    // printf("\nSum of 1 to %d is %d\n",n,sum);
+    // return 0;
+    sum = (n*(n+1))/2;
+    printf("\nSum of 1 to %d is %d\n",n,sum);
+
+}
